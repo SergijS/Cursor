b053c7f4a49c2dadf75a5e8e59fc4a6d1ba68bc8
SerhijShparaga <Serghey11@gmail.com>
2022-10-23 10:42:58 +0300

    replacing the algorithm

--- src/HW2/HW_2_Average_of_a_list_of_numbers.java
+++ src/HW2/HW_2_Average_of_a_list_of_numbers.java
@@ -4,9 +4,10 @@ public class HW_2_Average_of_a_list_of_numbers {
     public static void main(String[] args) {
 
         int [] array3 = new int[] {3, 5, 17, 25, 11};
-
-        int averange = array3.length / 2;
-
-        System.out.println(array3[averange]);
+        int averange = 0;
+        for (int i:array3){
+            averange += i;
+        }
+        System.out.println((double)averange / array3.length);
     }
 }
