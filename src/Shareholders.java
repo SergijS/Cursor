0074616819d97e316fcb037f22de9720a1cc8116
SergijS <107573014+SergijS@users.noreply.github.com>
2022-10-30 16:16:56 +0200

    HW3_Avto

---
+++ src/Shareholders.java
@@ -0,0 +1,18 @@
+package Avto;
+
+public class Shareholders extends Owner{
+    private double equityShare;
+
+    public double getEquityShare() {
+        return equityShare;
+    }
+
+    public void setEquityShare(double equityShare) {
+        this.equityShare = equityShare;
+    }
+
+    @Override
+    public void hello() {
+       System.out.println("Hello Shareholders, how are you?");
+    }
+}
