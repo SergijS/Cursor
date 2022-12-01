d67e0b4c8ed6dda6daa27b3edd91ce9c4a74a059
SergijS <107573014+SergijS@users.noreply.github.com>
2022-12-01 18:47:03 +0200

    Delete Main.java

--- Fsol/src/Main.java
+++
@@ -1,9 +0,0 @@
-import service.impl.UserServiceImpl;
-import view.impl.AdminMenu;
-
-public class Main {
-    public static void main(String[] args) {
-new AdminMenu(new UserServiceImpl()).show();
-
-    }
-}
